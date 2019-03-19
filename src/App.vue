2a8098f70a339acdfd1830c42e30367c1bca9178
zenve <zeonseservice@yandex.com>
2019-03-19 14:51:48 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@
     created() {
       this.axios.get('/api/seller').then((res) => {
         // console.log(res)
-        console.log('merge')
+        console.log('111')
         let {
           status: statu,
           data: {
