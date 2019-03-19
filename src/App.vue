4799ac5ac417e89359cb144c011f0bc9085a557e
zenve <zeonseservice@yandex.com>
2019-03-19 13:42:11 +0800

    console.log(2)

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,7 @@
     created() {
       this.axios.get('/api/seller').then((res) => {
         // console.log(res)
+        console.log(2)
         let {
           status: statu,
           data: {
